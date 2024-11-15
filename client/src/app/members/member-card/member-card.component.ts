84cd5f62016cd521257c9118e317f55b50ff28c7
Liyan <l.khandaqji@evokey.tech>
2024-11-15 22:39:10 +0300

    Routing into member details page

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Member } from '../../_models/member';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.css'
 })
--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-details',
   standalone: true,
   imports: [],
   templateUrl: './member-details.component.html',
-  styleUrl: './member-details.component.css'
+  styleUrl: './member-details.component.css',
 })
-export class MemberDetailsComponent {
+export class MemberDetailsComponent implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
 
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: (member) => (this.member = member),
+    });
+  }
 }
