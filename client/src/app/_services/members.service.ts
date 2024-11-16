c20a632cf1c06d3a3f6de59048cef3694d040d18
Liyan <l.khandaqji@evokey.tech>
2024-11-16 17:58:09 +0300

    manage state using servive

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,15 +10,22 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
 
   getMembers() {
-    return this.http.get<Member[]>(
-      this.baseUrl + 'users'
-      // this.getHttpOptions()
-    );
+    return this.http
+      .get<Member[]>(
+        this.baseUrl + 'users'
+        // this.getHttpOptions()
+      )
+      .subscribe({
+        next: (members) => this.members.set(members),
+      });
   }
 
   getMember(username: string) {
+    const member = this.members().find((x) => x.userName == username);
+    if (member !== undefined) return of(member); // this will return the member as observable
     return this.http.get<Member>(
       this.baseUrl + 'users/' + username
       // this.getHttpOptions() no need, jwt interceptor handles this
@@ -25,7 +33,13 @@ export class MembersService {
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => {
+        this.members.update((members) =>
+          members.map((m) => (m.userName === member.userName ? member : m))
+        );
+      })
+    );
   }
 
   getHttpOptions() {
@@ -39,3 +53,6 @@ export class MembersService {
 }
 
 // The HTTP headers are passed with the request to authenticate the user. The Authorization header allows the server to verify the user's identity by checking the token, ensuring that only authenticated users can access certain resources.
+
+//A signal is a special variable that holds a value and automatically notifies any part of your app that depends on it whenever its value changes.
+// It's used to reactively manage data and ensure your app updates efficiently when the state changes.
