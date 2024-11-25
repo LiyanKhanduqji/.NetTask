b7a2a1aef201c4a6b2a5cdc852a58d5630d26cc7
Liyan <l.khandaqji@evokey.tech>
2024-11-25 10:10:43 +0300

    Update user roles

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -14,6 +14,7 @@ import { jwtInterceptor } from './_interceptor/jwt.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { loadingInterceptor } from './_interceptor/loading.interceptor';
 import { TimeagoModule } from 'ngx-timeago';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -26,6 +27,10 @@ export const appConfig: ApplicationConfig = {
     provideToastr({
       positionClass: 'toast-bottom-right',
     }),
-    importProvidersFrom(NgxSpinnerModule, TimeagoModule.forRoot()),
+    importProvidersFrom(
+      NgxSpinnerModule,
+      TimeagoModule.forRoot(),
+      ModalModule.forRoot()
+    ),
   ],
 };
